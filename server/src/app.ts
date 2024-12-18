eb40e1a0eb2f871caecf8516ed49fb29b6b69f47
Phani2425 <mohantyphanibhusan@gmail.com>
2024-12-18 19:07:07 +0530

    Update CORS configuration to allow requests from any origin

--- server/src/app.ts
+++ server/src/app.ts
@@ -42,7 +42,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: [clientURL],
+    origin: '*',
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
